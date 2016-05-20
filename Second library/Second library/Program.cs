51de97a74a1d07e5a08e530d7314a924b0e96fdd
sandysci <ezeibesandra@gmail.com>
2016-05-20 09:42:50 -0700

    initial commit

---
+++ Second library/Second library/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using first;
+
+
+
+namespace Second_library
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            printer myprinterfirst = new printer();
+            myprinterfirst.Write("so happy it works");
+           // program2.read2("sandy thank Godoooo");
+        }
+    }
+    class program2
+    {
+
+
+       public static void read2(string my)
+        {
+
+            Console.WriteLine("--------------{0}--------------",my);
+            Console.ReadLine();
+
+        }
+    }
+
+}
+
---
+++ firstlibrary/firstlibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using first;
+using firstlibrary;
+
+ namespace firstlibrary
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            printer myprinter = new printer();
+            
+           // printer.Write("hello");
+            myprinter.Write("you");
+        }
+    }
+
+  /** class printer {
+
+       public void Write(string mystring)
+        {
+            Console.WriteLine("===== {0} =====",mystring);
+            Console.ReadLine();
+
+        }
+           
+           
+        }**/
+}
+namespace first
+{
+    
+
+   public class printer
+    {
+
+        public void Write(string mystring)
+        {
+            Console.WriteLine("===== {0} =====", mystring);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
+    
+
+
+    
+
